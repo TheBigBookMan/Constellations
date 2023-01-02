ccfd8640e6683954447745544d9360511932f8c9
Ben Smerd <bsmerd@adam.com.au>
2023-01-02 18:33:00 +1030

    update initial file structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// ? find a constellation API for information etc
+
+//? Probably use react spring and animejs
+
+// ? landing page idea, have a website about constellations in space--- because that can look cool with frontend effects use parralax scrolling with something like this, would be cool with a really space theme?
+
+//? --   https://twitter.com/uiuxadrian/status/1609543053034897409?s=20&t=w4l2COvDTu57kZ1ODScr4Q
+
+//? think can find a high defition photo somewhere of like that or something
+
+//? find constellations with SVG and can animate them in a cool way like click and the constellation dots then connect to make the constellations with lines--- would be SICKKKK
+
+function App() {
+  return (
+    <div>
+      <h1>Constellations</h1>
+    </div>
+  );
+}
+
+export default App;
